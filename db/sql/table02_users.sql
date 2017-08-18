f90788dc63c00c0919697b3491b88fb361c429f1
AJ Bahnken <aj.bahnken@procore.com>
2017-08-17 17:09:22 -0700

    added two missing columns to users

--- db/sql/table02_users.sql
+++ db/sql/table02_users.sql
@@ -1,6 +1,8 @@
 CREATE TABLE users (
-    id              serial PRIMARY KEY,
-    created         timestamp WITH time zone NOT NULL DEFAULT now(),
-    default_profile json
+    id                  serial PRIMARY KEY,
+    created             timestamp WITH time zone NOT NULL DEFAULT now(),
+    default_profile     json,
+    default_profile_enc json,
+    minilock_id         text   UNIQUE CHECK (40 <= LENGTH(minilock_id) AND LENGTH(minilock_id) <= 55)
 );
 ALTER TABLE users OWNER TO superuser;
