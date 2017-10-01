90f37552625175faa97857410a4582c05c8978de
Martin Yee <martytheemartian@yahoo.com>
2017-09-30 18:42:44 -0700

    Transferred Initial Pursuance Files
    Fixed ReadMe Typo & Static File Structure

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './store';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'),
+);
+
+if (module.hot) {
+  module.hot.accept();
+}
