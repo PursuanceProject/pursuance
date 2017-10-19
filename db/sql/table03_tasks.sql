f21545f69ac61e0d22f9a983342ce8de04861a94
Steve Phillips <steve@tryingtobeawesome.com>
2017-10-19 14:14:02 -0700

    Improved schemas: pursuances, users, tasks

--- db/sql/table03_tasks.sql
+++ db/sql/table03_tasks.sql
@@ -1,14 +1,16 @@
 CREATE TABLE tasks (
     id               serial    NOT NULL,
     pursuance_id     integer   NOT NULL REFERENCES pursuances ON DELETE CASCADE,
-    title            text      NOT NULL CHECK (LENGTH(title) <= 100) DEFAULT '',
-    title_enc        text      NOT NULL CHECK (LENGTH(title_enc) <= 2100) DEFAULT '',
-    description      text      NOT NULL CHECK (LENGTH(description) <= 10000) DEFAULT '',
-    description_enc  text      NOT NULL CHECK (LENGTH(description_enc) <= 12000) DEFAULT '',
-    deliverables     text      NOT NULL CHECK (LENGTH(deliverables) <= 2000) DEFAULT '',
-    deliverables_enc text      NOT NULL CHECK (LENGTH(deliverables_enc) <= 4000) DEFAULT '',
-    assignee         integer   REFERENCES users,
+    title            text      NOT NULL CHECK (LENGTH(title) <= 200) DEFAULT '',
+    title_enc        text      NOT NULL CHECK (LENGTH(title_enc) <= 2200) DEFAULT '',
+    deliverables     text      NOT NULL CHECK (LENGTH(deliverables) <= 10000) DEFAULT '',
+    deliverables_enc text      NOT NULL CHECK (LENGTH(deliverables_enc) <= 12000) DEFAULT '',
+    assigned_to      integer   REFERENCES users,
     created          timestamp WITH time zone NOT NULL DEFAULT now(),
-    PRIMARY KEY (task_id, pursuance_id)
+    PRIMARY KEY (id, pursuance_id),
+    CHECK ((LENGTH(title) > 0 OR LENGTH(title_enc) > 0)
+            AND
+            (LENGTH(title) = 0 OR LENGTH(title_enc) = 0)),
+    CHECK (LENGTH(deliverables) = 0 OR LENGTH(deliverables_enc) = 0)
 );
 ALTER TABLE tasks OWNER TO superuser;
