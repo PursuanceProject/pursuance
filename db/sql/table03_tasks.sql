f52849944c6c550804db150d53b5a9991730c45f
Steve Phillips <steve@tryingtobeawesome.com>
2017-10-21 01:08:53 -0700

    Schema: each task now points to its parent. Need not specify pursuance of parent task; assume it's the same as the task being saved

--- db/sql/table03_tasks.sql
+++ db/sql/table03_tasks.sql
@@ -7,6 +7,9 @@ CREATE TABLE tasks (
     deliverables_enc text      NOT NULL CHECK (LENGTH(deliverables_enc) <= 12000) DEFAULT '',
     assigned_to      integer   REFERENCES users,
     created          timestamp WITH time zone NOT NULL DEFAULT now(),
+    parent_task_id   integer,
+    parent_task_pursuance_id integer,
+    FOREIGN KEY (parent_task_id, parent_task_pursuance_id) REFERENCES tasks(id, pursuance_id) ON DELETE SET NULL,
     PRIMARY KEY (id, pursuance_id),
     CHECK ((LENGTH(title) > 0 OR LENGTH(title_enc) > 0)
             AND
@@ -14,3 +17,18 @@ CREATE TABLE tasks (
     CHECK (LENGTH(deliverables) = 0 OR LENGTH(deliverables_enc) = 0)
 );
 ALTER TABLE tasks OWNER TO superuser;
+
+CREATE OR REPLACE FUNCTION set_default_parent_task_pursuance_id() RETURNS trigger AS $$
+BEGIN
+    IF NEW.parent_task_pursuance_id IS NULL THEN
+        NEW.parent_task_pursuance_id := NEW.pursuance_id;
+    end IF;
+    RETURN NEW;
+END;
+$$ LANGUAGE PLPGSQL;
+ALTER FUNCTION set_default_parent_task_pursuance_id() OWNER TO superuser;
+
+CREATE TRIGGER trigger_set_default_parent_task_pursuance_id
+    BEFORE INSERT OR UPDATE ON tasks
+    FOR EACH ROW
+    EXECUTE PROCEDURE set_default_parent_task_pursuance_id();
