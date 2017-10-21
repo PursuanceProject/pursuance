967fde83fcbb9978791a063f575ea332ac62d71a
Steve Phillips <steve@tryingtobeawesome.com>
2017-10-21 01:54:48 -0700

    Added shortish-term fix to users table (removed minilock_id constraint0

--- db/sql/table02_users.sql
+++ db/sql/table02_users.sql
@@ -1,7 +1,7 @@
 CREATE TABLE users (
     id                  serial PRIMARY KEY,
     username            text   NOT NULL UNIQUE CONSTRAINT proper_username CHECK (username ~ '^[a-z][a-z0-9._-]{0,45}$'),
-    minilock_id         text   UNIQUE CHECK (40 <= LENGTH(minilock_id) AND LENGTH(minilock_id) <= 55),
+    minilock_id         text   NOT NULL DEFAULT '',
     default_profile     json   NOT NULL DEFAULT '{}',
     default_profile_enc text   NOT NULL DEFAULT '',
     created             timestamp WITH time zone NOT NULL DEFAULT now()
