d287d03b0e9921383bf8c32bcb066e42c894c840
Mo-Sho <muhama94@gmail.com>
2017-10-20 22:52:31 -0700

    .

--- src/components/Dashboard/TaskManager/TaskForm/TaskForm.js
+++ src/components/Dashboard/TaskManager/TaskForm/TaskForm.js
@@ -1,16 +1,26 @@
 import React, { Component } from 'react';
+import Assigner from './Assigner/Assigner';
 import './TaskForm.css';
+import generateId from '../../../../utils/generateId';
+console.log(generateId, 'generateId');
 
 class TaskForm extends Component {
+  componentWillMount() {
+    this.id = generateId('task')
+  }
+
+  onChange = (e) => console.log(e.target.value);
 
   render () {
+    console.log(this.id, 'id!!!!');
     return (
       <div className="task-form-container">
 
-        <form className="form-inline">
+        <form className="form-inline" name={this.id}>
           <div className="form-group">
             <label htmlFor="input-task-title">Task</label>
-            <input type="text" className="form-control" id="input-task-title" placeholder="Task Title" />
+            <input type="text" className="form-control" id="input-task-title" placeholder="Task Title" onChange={this.onChange}/>
+            <Assigner name={this.id + '-assigned_to'}/>
           </div>
           <button type="submit" className="btn btn-default">
             Submit
