d3cfac45edef034a433fce05ce19d6e697cb7438
Steve Phillips <steve@tryingtobeawesome.com>
2017-10-30 02:21:57 -0700

    Updated tasks schema/return value for both /rpc/subtasks_recursive_flat functions

--- db/sql/migration0003.sql
+++ db/sql/migration0003.sql
@@ -1,17 +1,14 @@
-CREATE OR REPLACE FUNCTION subtasks_recursive_flat(parent_gid text, subtask_pursuance_id integer) RETURNS TABLE(gid text, pursuance_id integer, id integer, title text, title_enc text, deliverables text, deliverables_enc text, assigned_to text, created timestamp WITH time zone) AS $$
+CREATE OR REPLACE FUNCTION subtasks_recursive_flat(parent_gid text, subtask_pursuance_id integer) RETURNS TABLE(gid text, pursuance_id integer, id integer, title text, title_enc text, deliverables text, deliverables_enc text, assigned_to text, due_date timestamp WITH time zone, created timestamp WITH time zone, parent_task_gid text) AS $$
     WITH RECURSIVE parent_task AS (
-            SELECT gid, pursuance_id, id, title, title_enc,
-                   deliverables, deliverables_enc, assigned_to, created
+            SELECT *
             FROM tasks WHERE parent_task_gid IS NOT DISTINCT FROM parent_gid
                          AND pursuance_id IS NOT DISTINCT FROM subtask_pursuance_id
         UNION ALL
-            SELECT tasks.gid, tasks.pursuance_id, tasks.id, tasks.title, tasks.title_enc,
-                   tasks.deliverables, tasks.deliverables_enc, tasks.assigned_to, tasks.created
+            SELECT tasks.*
             FROM tasks, parent_task WHERE tasks.parent_task_gid IS NOT DISTINCT FROM parent_task.gid
                                       AND tasks.pursuance_id IS NOT DISTINCT FROM parent_task.pursuance_id
     )
-    SELECT gid, pursuance_id, id, title, title_enc,
-           deliverables, deliverables_enc, assigned_to, created
+    SELECT *
     FROM parent_task;
 $$ LANGUAGE SQL STABLE;
 ALTER FUNCTION subtasks_recursive_flat(parent_gid text, subtask_pursuance_id integer) OWNER TO superuser;
