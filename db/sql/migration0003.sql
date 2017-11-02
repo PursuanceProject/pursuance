cda74051651d6596c5f38cf459539f4758e89ef1
Steve Phillips <steve@tryingtobeawesome.com>
2017-11-01 19:53:03 -0700

    Moved tasks.assigned_to_pursuance_id addition into tasks table directly
    Former-commit-id: 5f21c04d98eaf7c7e72c70b54abc4b6f8cdf8654

--- db/sql/migration0003.sql
+++ db/sql/migration0003.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE FUNCTION subtasks_recursive_flat(parent_gid text, subtask_pursuance_id integer) RETURNS TABLE(gid text, pursuance_id integer, id integer, title text, title_enc text, deliverables text, deliverables_enc text, assigned_to text, due_date timestamp WITH time zone, created timestamp WITH time zone, parent_task_gid text, assigned_to_pursuance_id integer) AS $$
+CREATE OR REPLACE FUNCTION subtasks_recursive_flat(parent_gid text, subtask_pursuance_id integer) RETURNS TABLE(gid text, pursuance_id integer, id integer, title text, title_enc text, deliverables text, deliverables_enc text, assigned_to text, assigned_to_pursuance_id integer, due_date timestamp WITH time zone, created timestamp WITH time zone, parent_task_gid text) AS $$
     WITH RECURSIVE parent_task AS (
             SELECT *
             FROM tasks WHERE parent_task_gid IS NOT DISTINCT FROM parent_gid
--- db/sql/migration0004_add_column_tasks_assigned_to_pursuance_id.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE tasks
-    ADD COLUMN assigned_to_pursuance_id integer REFERENCES pursuances(id);
