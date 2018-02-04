dc0d762bbf1ca44526691532306d08265996dde2
Val <dgm97@cornell.edu>
2018-02-03 21:22:57 -0800

    order public pursuances

---
+++ src/reducers/publicOrderReducer.js
@@ -0,0 +1,9 @@
+export default function(state = "Most Recent", action) {
+  switch (action.type) {
+    case 'SET_PUBLIC_ORDER':
+    	state = action.publicOrder;
+      return state;
+    default:
+      return state;
+  }
+}
