9352a43d95d8937fa63bf3914838b6a89a8bd896
Val <dgm97@cornell.edu>
2018-02-07 19:28:47 -0800

    refine public order reducer

--- src/reducers/publicOrderReducer.js
+++ src/reducers/publicOrderReducer.js
@@ -1,7 +1,7 @@
-export default function(state = "Most Recent", action) {
+export default function(state = 'Most Recent', action) {
   switch (action.type) {
     case 'SET_PUBLIC_ORDER':
-    	state = action.publicOrder;
+      state = action.publicOrder;
       return state;
     default:
       return state;
