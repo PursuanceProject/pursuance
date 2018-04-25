38274625d7408cdf1db5ee603c425743bde261f8
Steve Phillips <steve@tryingtobeawesome.com>
2018-04-25 00:28:00 -0700

    AboutView: tweaked styling, verbiage.  Whitespace.

--- src/components/Content/Pursuance/views/AboutView.js
+++ src/components/Content/Pursuance/views/AboutView.js
@@ -6,19 +6,18 @@ class AboutView extends Component {
 
   render() {
     const {currentPursuanceId, pursuances} = this.props;
-    const p
-    = (pursuances[currentPursuanceId] !== undefined)
-    ? pursuances[currentPursuanceId] : "";
+    const p = (pursuances[currentPursuanceId] !== undefined) ?
+          pursuances[currentPursuanceId] : "";
     return (
-      <div className="about">
-        <h1>{p.name}</h1>
-        <h2>{p.mission}</h2>
-        <h2>{p.about}</h2>
+      <div className="content about">
+        <h1>About This Pursuance</h1>
+        <h2>Name: {p.name}</h2>
+        <h4>Mission: {p.mission}</h4>
       </div>
     );
   }
 }
 
-export default connect(({currentPursuanceId, pursuances}) =>
-  ({ currentPursuanceId, pursuances})
-  )(AboutView);
+export default connect(({ currentPursuanceId, pursuances }) =>
+  ({ currentPursuanceId, pursuances })
+)(AboutView);
