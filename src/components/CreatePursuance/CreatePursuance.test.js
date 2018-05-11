16b21802e2097fc0bbefe0c8ec56998160a68d51
Kurt Wall <kwall@kurtwerks.com>
2018-05-10 23:20:19 -0700

    Apply review comments

--- src/components/CreatePursuance/CreatePursuance.test.js
+++ src/components/CreatePursuance/CreatePursuance.test.js
@@ -1,4 +1,4 @@
-describe('Creaing a pursuance', function() {
-  it('should do all off the right things');
+describe('Creating a pursuance', function() {
+  it('should do all of the right things');
   it('should not do any of the unright things');
 });
