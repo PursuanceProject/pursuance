177bcea1c8482c40c8c9d4465d5e21689879a8cf
Steve Phillips <steve@tryingtobeawesome.com>
2018-05-23 15:00:06 -0700

    Better Firefox compatibility + better-looking word wrappiness
    Contributes to #175

--- src/components/Content/Pursuance/PursuanceMenuItem.js
+++ src/components/Content/Pursuance/PursuanceMenuItem.js
@@ -2,11 +2,11 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import './PursuanceMenuItem.css';
 
-const PursuanceMenuItem = ({ pursuanceId, label, action, icon, location, className }) => {
+const PursuanceMenuItem = ({ pursuanceId, label, action, icon, location, className = '' }) => {
   const rootPath = `/pursuance/${pursuanceId}`;
   return (
     <NavLink
-      className={"menu-item " + (className ? className : '')}
+      className={"menu-item " + className}
       to={`${rootPath}/${action}`}
       isActive={
         () =>
