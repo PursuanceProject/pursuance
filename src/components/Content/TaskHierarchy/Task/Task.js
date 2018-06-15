c1e3c07bfeab55505935c245e8238a04b7f1e29d
colinfruit <colin.fruit@yahoo.com>
2018-06-14 21:07:39 -0700

    finish task reassignment functionality

--- src/components/Content/TaskHierarchy/Task/Task.js
+++ src/components/Content/TaskHierarchy/Task/Task.js
@@ -26,7 +26,6 @@ import {
 
 const taskSource = {
   beginDrag(props, monitor, component) {
-    // Return the data describing the dragged item
     const { taskData } = props;
     return taskData;
   },
