65850adae40a4abfec15fac92cda2f1b5f6952c1
Steve Phillips <steve@tryingtobeawesome.com>
2018-06-21 11:17:02 -0700

    Added Object.values polyfill, aiding iOS compatibility
    Contributes to #208

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import './utils/detect_browser';
 import './utils/origin_polyfill';
+import './utils/object_values_polyfill';
 import store from './store';
 import App from './App';
 import './index.css';
