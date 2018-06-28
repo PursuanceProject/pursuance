f7cf83289474ed4e2902f095c7c0652b3f6c14f8
4xdk <dabi@startmail.com>
2018-06-28 21:35:54 +0100

    wip

--- src/components/NavBar/JumpToPursuance/JumpToPursuance.js
+++ src/components/NavBar/JumpToPursuance/JumpToPursuance.js
@@ -1,20 +1,70 @@
-import React from 'react';
-import { MenuItem } from 'react-bootstrap';
+import React, { Component } from 'react';
+import { DropdownButton, MenuItem } from 'react-bootstrap';
 
-const produceOptions = (pursuances) => {
-  const pursuanceArr = Object.values(pursuances);
-  pursuanceArr.sort((p1, p2) => {
-    return p1.name.toLowerCase().localeCompare(p2.name.toLowerCase());
-  });
-  return pursuanceArr.map((pursuance) => (
-    <MenuItem
-      eventKey={pursuance.id}
-      key={pursuance.id}
-      value={pursuance.id}
-    >
-      {pursuance.name}
-    </MenuItem>
-  ));
+class JumpToPursuance extends Component {
+  produceOptions = (pursuances) => {
+    const pursuanceArr = Object.values(pursuances);
+    pursuanceArr.sort((p1, p2) => {
+      return p1.name.toLowerCase().localeCompare(p2.name.toLowerCase());
+    });
+
+    return pursuanceArr.map((pursuance) => (
+      <MenuItem
+        eventKey={pursuance.id}
+        key={pursuance.id}
+        value={pursuance.id}
+      >
+        {pursuance.name}
+      </MenuItem>
+    ));
+  }
+
+  render() {
+      return (
+        // <NavDropdown id="jump-to-pursuance" title={this.showCurrentPursuance(pursuances)} onSelect={this.handleJumpToPursuance}>
+        // {jumpToPursuanceOptions(pursuances)}
+        // </NavDropdown>
+        <ul className="nav-pursuances">
+          <div class="dropdown open btn-group">
+            <DropdownButton
+              id="task-status-dropdown"
+              title={this.props.title}
+              noCaret>
+              {this.produceOptions(this.props.pursuances)}
+            </DropdownButton>
+            <div className="edit-icon-ctn">
+              tip
+            </div>
+          </div>
+        </ul>
+      )
+  }
 }
 
-export default produceOptions;
+export default JumpToPursuance;
+
+
+// <div className={"task-status-ctn task-status-" + status + " hide-small"}>
+//         <DropdownButton
+//           id="task-status-dropdown"
+//           title={this.getCurrentStatus()}
+//           noCaret>
+//           {this.getDropDownItems()}
+//         </DropdownButton>
+//         {STATUS_IMAGES[status] && <img src={`/assets/img/${status}.gif`} alt={`Status: ${status}`} />}
+//         <div className="edit-icon-ctn">
+//           <TiPencil id="task-edit-icon" size={18} />
+//         </div>
+//       </div>
+
+
+// <ul role="menu" class="dropdown-menu" aria-labelledby="task-status-dropdown">
+//   <li role="presentation" class=""><a role="menuitem" tabindex="-1" href="#">Started</a></li>
+//   <li role="presentation" class=""><a role="menuitem" tabindex="-1" href="#">Working On</a></li>
+//   <li role="presentation" class=""><a role="menuitem" tabindex="-1" href="#">Help Wanted</a></li>
+//   <li role="presentation" class=""><a role="menuitem" tabindex="-1" href="#">Ready For Review</a></li>
+//   <li role="presentation" class=""><a role="menuitem" tabindex="-1" href="#">Reviewing</a></li>
+//   <li role="presentation" class=""><a role="menuitem" tabindex="-1" href="#">Done</a></li>
+// </ul>
+
+// <TiPencil id="task-edit-icon" size={18} />
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link, withRouter } from 'react-router-dom';
-import { Navbar, NavDropdown, NavItem, OverlayTrigger, Tooltip } from 'react-bootstrap';
+import { Navbar, NavItem, Nav, OverlayTrigger, Tooltip } from 'react-bootstrap';
 import FaBell from 'react-icons/lib/fa/bell';
 import SignUp from './SignUp/SignUp';
 import LogIn from './LogIn/LogIn';
 import NotificationsModal from './NotificationsModal/NotificationsModal';
 import UserSettingsPopover from './UserSettingsPopover';
-import jumpToPursuanceOptions from './JumpToPursuance/JumpToPursuance';
+import JumpToPursuance from './JumpToPursuance/JumpToPursuance';
 import { getPursuances, setCurrentPursuance } from '../../actions';
 import './NavBar.css';
 
@@ -48,57 +48,70 @@ class NavBar extends Component {
     const { user, pursuances } = this.props;
     const { authenticated, username, contributionPoints } = user;
     return (
-      <nav className="nav-container">
+      <Navbar collapseOnSelect>
         <Navbar.Header>
           <Navbar.Brand>
             <Link to="/">Pursuance</Link>
           </Navbar.Brand>
         </Navbar.Header>
-        <Link to="/dashboard">Dashboard</Link>
-        <div className="nav-center">
-        {authenticated && (
-          <NavDropdown
-            id="jump-to-pursuance"
-            title={this.showCurrentPursuance(pursuances)}
-            onSelect={this.handleJumpToPursuance}
-          >
-            {jumpToPursuanceOptions(pursuances)}
-          </NavDropdown>
-        )}
-        </div>
-        <div className="nav-pull-right">
-        {!authenticated &&
-          (<span>
-            <NavItem data-toggle="modal" data-target="#sign-up-modal">
-              Sign Up
-            </NavItem>
-            <SignUp />
-            <NavItem data-toggle="modal" data-target="#log-in-modal">
-              Log In
-            </NavItem>
-            <LogIn />
-          </span>
-          )
-        }
-        {authenticated &&
-          (<span>
-            <OverlayTrigger
-              placement="bottom"
-              overlay={this.getTooltip()}>
-              <NavItem data-toggle="modal"
-                data-target="#notifications-modal">
-                <FaBell size={24} />
-              </NavItem>
-            </OverlayTrigger>
-            <NotificationsModal
-                onIncreaseContributionAmount={this.props.onIncreaseContributionAmount}
-                onRemoveNotification={this.props.onRemoveNotification} />
-            <UserSettingsPopover username={username} contributionPoints={contributionPoints} />
-          </span>
-          )
-        }
-        </div>
-      </nav>
+        <Navbar.Collapse>
+          <ul className="nav navbar-nav">
+            <li role="presentation">
+              <Link to="/dashboard">Dashboard</Link>
+            </li>
+          </ul>
+          <Nav pullRight>
+            {
+              !authenticated &&
+              (
+                <NavItem data-toggle="modal" data-target="#sign-up-modal">
+                  Sign Up
+                </NavItem>
+              )
+            }
+            {!authenticated && <SignUp />}
+            {
+              !authenticated &&
+              (
+                <NavItem data-toggle="modal" data-target="#log-in-modal">
+                  Log In
+                </NavItem>
+              )
+            }
+            {!authenticated && <LogIn />}
+            {
+              authenticated &&
+              (
+                <OverlayTrigger
+                  placement="bottom"
+                  overlay={this.getTooltip()}>
+                  <NavItem data-toggle="modal"
+                    data-target="#notifications-modal">
+                    <FaBell size={24} />
+                  </NavItem>
+                </OverlayTrigger>
+              )
+            }
+            {
+              authenticated &&
+                <NotificationsModal
+                  onIncreaseContributionAmount={this.props.onIncreaseContributionAmount}
+                  onRemoveNotification={this.props.onRemoveNotification} />
+            }
+            {
+              authenticated &&
+              (
+                <NavItem>
+                  <UserSettingsPopover username={username} contributionPoints={contributionPoints} />
+                </NavItem>
+              )
+            }
+          </Nav>
+          {authenticated &&
+            <JumpToPursuance title={this.showCurrentPursuance(pursuances)} onSelect={this.handleJumpToPursuance} pursuances={pursuances}/>
+          }
+        </Navbar.Collapse>
+      </Navbar> 
     );
   }
 }
